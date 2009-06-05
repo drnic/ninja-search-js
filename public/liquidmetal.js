8b7dcd472739c618c6ecd16bb25b8b8bc12a37d8
Dr Nic Williams <drnicwilliams@gmail.com>
2009-06-05 21:58:49 +1000

    fixed liquidmetal to use alternate for() loop syntax since prototypejs is polluting arrays with an extra element

--- public/liquidmetal.js
+++ public/liquidmetal.js
@@ -28,10 +28,9 @@ var LiquidMetal = function() {
       var scores = this.buildScoreArray(string, abbreviation);
 
       var sum = 0.0;
-      for (var i in scores) {
+      for (var i=0; i < scores.length; i++) {
         sum += scores[i];
       }
-
       return (sum / scores.length);
     },
 
@@ -42,7 +41,7 @@ var LiquidMetal = function() {
 
       var lastIndex = -1;
       var started = false;
-      for (var i in chars) {
+      for (var i=0; i < chars.length; i++) {
         var c = chars[i];
         var index = lower.indexOf(c, lastIndex+1);
         if (index < 0) return fillArray(scores, SCORE_NO_MATCH);
@@ -62,7 +61,6 @@ var LiquidMetal = function() {
         scores[index] = SCORE_MATCH;
         lastIndex = index;
       }
-
       var trailingScore = started ? SCORE_TRAILING_BUT_STARTED : SCORE_TRAILING;
       fillArray(scores, trailingScore, lastIndex+1);
       return scores;
